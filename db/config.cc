5725145e4a82c8692fa315f418d3ad29bbd17656
Mikołaj Grzebieluch <mikolaj.grzebieluch@scylladb.com>
2022-07-11 10:47:13 +0200

    db: config: add GROUP0_TABLES feature flag
    Add experimental flag 'group0-tables' for enabling GROUP0_TABLES feature.
    This feature requires raft group0, thus enabling it without RAFT will cause an error.

--- db/config.cc
+++ db/config.cc
@@ -1014,7 +1014,8 @@ db::fs::path db::config::get_conf_sub(db::fs::path sub) {
 bool db::config::check_experimental(experimental_features_t::feature f) const {
     if (experimental()
         && f != experimental_features_t::feature::UNUSED
-        && f != experimental_features_t::feature::RAFT) {
+        && f != experimental_features_t::feature::RAFT
+        && f != experimental_features_t::feature::GROUP0_TABLES) {
             return true;
     }
     const auto& optval = experimental_features();
@@ -1057,6 +1058,7 @@ std::map<sstring, db::experimental_features_t::feature> db::experimental_feature
         {"alternator-streams", feature::ALTERNATOR_STREAMS},
         {"alternator-ttl", feature::ALTERNATOR_TTL},
         {"raft", feature::RAFT},
+        {"group0-tables", feature::GROUP0_TABLES},
         {"keyspace-storage-options", feature::KEYSPACE_STORAGE_OPTIONS},
     };
 }
